846637c85ea4ebc4c8428d96089df1031e0dacf4
Sunho Kim <ksunhokim123@gmail.com>
2024-06-24 06:48:31 -0700

    Pipeline

--- src/localai/build.py
+++ src/localai/build.py
@@ -15,18 +15,18 @@ def build_localai():
         print(f"Error during PyInstaller build: {e}")
         return
 
-    # Define the source and destination paths
-    src = os.path.join('dist', 'localai')
-    dst = os.path.join('..', '..', 'release', 'app', 'localai')
-
-    # Copy the built files to the destination directory
-    try:
-        if os.path.exists(dst):
-            shutil.rmtree(dst)
-        shutil.copytree(src, dst)
-        print(f"Successfully copied {src} to {dst}")
-    except Exception as e:
-        print(f"Error during copying files: {e}")
+    # # Define the source and destination paths
+    # src = os.path.join('dist', 'localai')
+    # dst = os.path.join('..', '..', 'release', 'app', 'localai')
+    #
+    # # Copy the built files to the destination directory
+    # try:
+    #     if os.path.exists(dst):
+    #         shutil.rmtree(dst)
+    #     shutil.copytree(src, dst)
+    #     print(f"Successfully copied {src} to {dst}")
+    # except Exception as e:
+    #     print(f"Error during copying files: {e}")
 
 if __name__ == "__main__":
     build_localai()
