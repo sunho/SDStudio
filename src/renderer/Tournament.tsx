c1e1c1418eac425455f12790c0cde88a5fd891f0
Sunho Kim <ksunhokim123@gmail.com>
2024-06-26 23:52:06 -0700

    Add more tournament options

--- src/renderer/Tournament.tsx
+++ src/renderer/Tournament.tsx
@@ -22,31 +22,84 @@ interface TournamentProps {
 const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
   const { curSession, pushMessage, pushDialog } = useContext(AppContext)!;
   const [images, setImages] = useState<string[]>([]);
-  const [matches, setMatches] = useState<Match[]>([]);
-  const matchRes = useRef<number[]>([]);
-  const [winner, setWinner] = useState(-1);
+  const [players, setPlayers] = useState<string[]>([]);
   const lock = useRef(false);
   const [finalRank, setFinalRank] = useState(-1);
-  const [mi, setMi] = useState(0);
-  const setNextMatch = () => {
-    const [finalizedRank, newMatches] = gameService.nextMatch(scene.game!);
-    setMi(0);
-    if (newMatches) {
-      setMatches(newMatches);
-      setFinalRank(finalizedRank);
-    } else {
-      setMatches([]);
-      setFinalRank(finalizedRank);
+  const loadRoundInitial = () => {
+    const [finalizedRank, newRound] = gameService.nextRound(scene.game!);
+    if (!scene.round) {
+      scene.round = newRound;
+      sessionUpdated();
     }
+    if (newRound) {
+      const round = scene.round!;
+      setPlayers([round.players[round.curPlayer].path, round.players[round.curPlayer+1].path]);
+    }
+    setFinalRank(finalizedRank);
+  };
+  const finalizeMatch = () => {
+    let wins = 0;
+    let loses = 0;
+    const round = scene.round!;
+    if (round.players.length % 2 === 1) {
+      round.winMask[round.players.length - 1] = true;
+    }
+    for (let i = 0; i < round.players.length; i++) {
+      if (round.winMask[i]) {
+        wins++;
+      } else {
+        loses++;
+      }
+    }
+    const roundRank = round.players[0].rank;
+    const winRank = roundRank - loses;
+    for (let i = 0; i < round.players.length; i++) {
+      if (round.winMask[i]) {
+        round.players[i].rank = winRank;
+      }
+    }
+    if (winRank === 0) {
+      pushDialog({
+        type: 'yes-only',
+        text: '1위가 결정되었습니다. 여기서 멈춰도 됩니다.',
+      });
+    }
+    gameUpdated();
+  };
+  const nextMatch = () => {
+    if (lock.current)
+      return;
+    setPlayers([]);
+    lock.current = true;
+    const round = scene.round!;
+    round.curPlayer += 2;
+    if (round.curPlayer + 1 >= round.players.length) {
+      finalizeMatch();
+      const [finalizedRank, newRound] = gameService.nextRound(scene.game!);
+      if (newRound) {
+        scene.round = newRound;
+        setPlayers([newRound.players[0].path, newRound.players[1].path]);
+      }
+      setFinalRank(finalizedRank);
+      sessionUpdated();
+      lock.current = false;
+      return;
+    }
+    setPlayers([round.players[round.curPlayer].path, round.players[round.curPlayer+1].path]);
+    sessionUpdated();
+    lock.current = false;
   };
   const gameUpdated = () => {
     gameService.gameUpdated(curSession!, scene);
+    sessionUpdated();
+  };
+  const sessionUpdated = () => {
     sessionService.markUpdated(curSession!.name);
   };
   useEffect(() => {
-    setMatches([]);
+    setPlayers([]);
+    setImages([]);
     setFinalRank(-1);
-    setWinner(-1);
     (async () => {
       try {
         if (!scene.game) {
@@ -60,38 +113,32 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
             text: '새로운 이미지가 추가되었습니다. 순위를 초기화 해주세요.',
           });
         }
-        setNextMatch();
+        loadRoundInitial();
       } catch (e: any) {
         pushMessage('Error: ' + e.message);
       }
     })();
   }, [scene]);
-  const finalizeMatch = () => {
-    for (let i = 0; i < matches.length; i++) {
-      matches[i].players[matchRes.current[i]].rank = matches[i].winRank;
-    }
-  };
   useEffect(() => {
-    if (matches.length) {
+    const round = scene.round!;
+    if (players.length) {
       (async () => {
         try {
-          const p0 = await imageService.fetchImage(matches[mi].players[0].path);
-          const p1 = await imageService.fetchImage(matches[mi].players[1].path);
+          const p0 = await imageService.fetchImage(players[0]);
+          const p1 = await imageService.fetchImage(players[1]);
           setImages([p0, p1]);
-          setWinner(-1);
         } catch (e: any) {
           pushMessage('Image load error: ' + e.message);
           setImages([]);
         }
       })();
-    } else if (scene.game && scene.game.length && matches.length === 0) {
+    } else if (scene.game && scene.game.length && players.length === 0) {
       const first = scene.game.find((p) => p.rank === 0);
       if (first) {
         (async () => {
           try {
             const p0 = await imageService.fetchImage(first.path);
             setImages([p0]);
-            setWinner(-1);
           } catch (e: any) {
             pushMessage('Image load error: ' + e.message);
             setImages([]);
@@ -103,48 +150,18 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
     } else {
       setImages([]);
     }
-  }, [matches, mi]);
-  const selectPlayer = (index: any) => {
-    if (lock.current)
-      return;
-    setWinner(index);
-    lock.current = true;
-    const goNext= async () => {
-      try {
-        matchRes.current[mi] = index;
-        if (mi == matches.length - 1) {
-          finalizeMatch();
-          gameUpdated();
-          if (matches[mi].winRank === 0) {
-            pushDialog({
-              type: 'yes-only',
-              text: '1위가 결정되었습니다. 여기서 멈춰도 됩니다.',
-            });
-          }
-          setNextMatch();
-        } else {
-          setMi(mi + 1);
-        }
-      } catch (e: any) {
-        pushMessage('Error: ' + e.message);
-      } finally {
-        lock.current = false;
-      }
-    };
-    goNext();
-  };
+  }, [players]);
   const resetRanks = () => {
     pushDialog({
       type: 'confirm',
       text: '정말로 순위를 초기화하시겠습니까?',
       callback: async () => {
-        setMatches([]);
-        matchRes.current = [];
+        setPlayers([]);
         setFinalRank(-1);
-        setWinner(-1);
         try {
           scene.game = await gameService.createGame(path);
-          setNextMatch();
+          scene.round = undefined;
+          loadRoundInitial();
         } catch (e: any) {
           pushMessage('Error: ' + e.message);
         }
@@ -152,40 +169,37 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
     });
   };
   const reroll = () => {
-    if (matches.length <= 1) {
+    const round = scene.round!;
+    if (round.players.length <= 1) {
       return;
     }
-    const players = matches.slice(mi).map((m) => m.players).flat();
+    const players = round.players.slice(round.curPlayer);
     shuffleArray(players);
-    const newMatches = [];
-    for (let i = 0; i < players.length; i += 2) {
-      newMatches.push({
-        players: [players[i], players[i + 1]],
-        winRank: matches[mi].winRank,
-        loseRank: matches[mi].loseRank,
-      });
-    }
-    setMatches(matches.slice(0, mi).concat(newMatches));
+    round.players = round.players.slice(0, round.curPlayer).concat(players);
+    setPlayers([players[round.curPlayer].path, players[round.curPlayer+1].path]);
+    sessionUpdated();
   };
   const getCurWinRank = () => {
-    if (matches[mi].winRank === 0) {
+    const round = scene.round!;
+    if (round.players.length === 2) {
       return '결승전';
     }
-    if (matches[mi].winRank === 1) {
+    if (round.players.length <= 5) {
       return '준결승전';
     }
-    return `${matches[mi].winRank + 1}강`;
+    return `${Math.floor(round.players.length/2)}강`;
   };
   const showFolder = async () => {
     await invoke('show-file', path);
   };
+  const round = scene.round!;
   return (
     <div className="flex flex-col w-full h-full">
       <div className="p-4 flex flex-none gap-2 items-center">
-        {!!matches.length ? (
+        {!!players.length ? (
           <span className="font-bold text-xl">
-            {finalRank + 1}위 결정 이상형 월드컵 {getCurWinRank()} ({mi + 1}/
-            {matches.length})
+            {finalRank + 1}위 결정 이상형 월드컵 {getCurWinRank()} ({Math.floor(round.curPlayer/2) + 1}/
+            {Math.floor(round.players.length/2)})
           </span>
         ) : (
           <span className="font-bold text-xl">모든 순위가 확정되었습니다</span>
@@ -200,8 +214,11 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
         </button>
         <button
           onClick={() => {
-            if (winner === -1 && !lock.current && mi !== 0)
-              setMi(mi - 1);
+            if (!lock.current && round.curPlayer !== 0) {
+              setPlayers([]);
+              round.curPlayer -= 2;
+              setPlayers([round.players[round.curPlayer].path, round.players[round.curPlayer+1].path]);
+            }
           }}
           className={`${roundButton} bg-gray-500`}
         >
@@ -210,23 +227,36 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
         <button className={`${roundButton} bg-orange-400`} onClick={reroll}>
           대진 리롤
         </button>
-        {/* <button */}
-        {/*   className={`${roundButton} ${fastmode ? 'bg-orange-400' : 'bg-green-500'}`} */}
-        {/*   onClick={() => { */}
-        {/*     setFastmode(!fastmode); */}
-        {/*   }} */}
-        {/* > */}
-        {/*   {fastmode ? '일반모드 켜기' : '페스트모드 켜기'} */}
-        {/* </button> */}
+        <button className={`${roundButton} bg-orange-400`} onClick={() => {
+          if (!lock.current){
+            round.winMask[round.curPlayer] = false;
+            round.winMask[round.curPlayer+1] = false;
+            nextMatch();
+          }
+        }}>
+          둘다 패배 처리
+        </button>
+        <button className={`${roundButton} bg-orange-400`} onClick={() => {
+          if (!lock.current){
+            round.winMask[round.curPlayer] = true;
+            round.winMask[round.curPlayer+1] = true;
+            nextMatch();
+          }
+        }}>
+          둘다 승리 처리
+        </button>
       </div>
       <div className="flex-1 w-full overflow-hidden">
-        {!!(matches.length && images.length) && (
+        {!!(players.length && images.length) && (
           <div className="h-full float-view-comp-width flex">
             <div className="flex-1 justify-center items-center flex">
               <img
                 onClick={() => {
-                  if (winner === -1)
-                    selectPlayer(0);
+                  if (!lock.current){
+                    round.winMask[round.curPlayer] = true;
+                    round.winMask[round.curPlayer+1] = false;
+                    nextMatch();
+                  }
                 }}
                 className={
                   'active:brightness-90 hover:brightness-95 cursor-pointer imageSmall '
@@ -234,7 +264,7 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
                 src={images[0]}
                 alt={encodeContextAlt({
                   type: 'image',
-                  path: matches[mi].players[0].path
+                  path: players[0]
                 })}
               />
             </div>
@@ -242,8 +272,11 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
             <div className="flex-1 justify-center items-cetner flex">
               <img
                 onClick={() => {
-                  if (winner === -1)
-                    selectPlayer(1);
+                  if (!lock.current){
+                    round.winMask[round.curPlayer] = false;
+                    round.winMask[round.curPlayer+1] = true;
+                    nextMatch();
+                  }
                 }}
                 className={
                   'active:brightness-90 hover:brightness-95 cursor-pointer imageSmall flex-1 '
@@ -251,7 +284,7 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
                 src={images[1]}
                 alt={encodeContextAlt({
                   type: 'image',
-                  path: matches[mi].players[1].path
+                  path: players[1]
                 })}
               />
             </div>
@@ -260,7 +293,7 @@ const Tournament = ({ scene, path, onFilenameChange }: TournamentProps) => {
         {!!(
           scene.game &&
           scene.game.length &&
-          matches.length === 0 &&
+          players.length === 0 &&
           images.length
         ) && (
           <div className="h-full w-full">
