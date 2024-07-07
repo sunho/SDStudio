ba2743a2d7e004566583f95f38156a8c92672ebe
Sunho Kim <ksunhokim123@gmail.com>
2024-07-06 23:25:33 -0700

    push

--- capacitor.config.deploy.ts
+++ capacitor.config.deploy.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.sunho.SDStudio',
   appName: 'SDStudio',
   webDir: 'dist',
-  server: {
-    url: "http://192.168.0.116:5173",
-    cleartext: true
-  },
   android: { allowMixedContent: true },
   plugins: {
     CapacitorHttp: {
