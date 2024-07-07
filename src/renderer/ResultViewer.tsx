6a0575eefc800c5e395e7a7b8e8100aea16d78af
Sunho Kim <ksunhokim123@gmail.com>
2024-07-06 22:34:57 -0700

    ng

--- src/renderer/ResultViewer.tsx
+++ src/renderer/ResultViewer.tsx
@@ -294,7 +294,7 @@ interface ResultDetailViewButton {
 
 interface ResultDetailViewProps {
   scene: GenericScene;
-  paths: string[];
+  getPaths: ()=>string[];
   initialSelectedIndex: number;
   buttons: ResultDetailViewButton[];
   onClose: () => void;
@@ -302,12 +302,13 @@ interface ResultDetailViewProps {
 const ResultDetailView = ({
   scene,
   buttons,
-  paths,
+  getPaths,
   initialSelectedIndex,
   onClose,
 }: ResultDetailViewProps) => {
   const { curSession, selectedPreset, pushDialog } = useContext(AppContext)!;
   const [selectedIndex, setSelectedIndex] = useState<number>(initialSelectedIndex);
+  const [paths, setPaths] = useState<string[]>(getPaths());
   const [filename, setFilename] = useState<string>(paths[selectedIndex].split('/').pop()!);
   const [image, setImage] = useState<string | undefined>(undefined);
   const watchedImages = useRef(new Set<string>());
@@ -357,13 +358,44 @@ const ResultDetailView = ({
       forceUpdate({});
     };
     fetchImage();
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        setSelectedIndex((selectedIndex - 1 + paths.length) % paths.length);
+      } else if (e.key === 'ArrowRight') {
+        setSelectedIndex((selectedIndex + 1) % paths.length);
+      } else if (e.key === 'Delete' || e.key === 'Backspace') {
+        pushDialog({
+          type: 'confirm',
+          text: '정말로 파일을 삭제하시겠습니까?',
+          callback: async () => {
+            await deleteImageFiles(curSession!, [paths[selectedIndex]], scene);
+          },
+        });
+      }
+    };
+    const refreshPaths = () => {
+      const newPaths = getPaths();
+      if (newPaths.length === 0)
+        onClose();
+      else {
+        let newIndex = newPaths.indexOf(imageService.getOutputDir(curSession!, scene) + '/' + filename)
+        if (newIndex !== -1) {
+          setSelectedIndex(newIndex);
+        }
+        setPaths(newPaths);
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
     sessionService.addEventListener('main-image-updated', rerender);
     imageService.addEventListener('image-cache-invalidated', fetchImage);
+    gameService.addEventListener('updated', refreshPaths);
     return () => {
+      window.removeEventListener('keydown', handleKeyDown);
       sessionService.removeEventListener('main-image-updated', rerender);
       imageService.removeEventListener('image-cache-invalidated', fetchImage);
+      gameService.removeEventListener('updated', refreshPaths);
     };
-  }, [selectedIndex]);
+  }, [selectedIndex, paths]);
 
   useEffect(() => {
     return () => {
@@ -401,7 +433,6 @@ const ResultDetailView = ({
                   text: '정말로 파일을 삭제하시겠습니까?',
                   callback: async () => {
                     await deleteImageFiles(curSession!, [paths[selectedIndex]]);
-                    onClose();
                   },
                 })
               }}
@@ -541,6 +572,7 @@ const ResultViewer = forwardRef<ResultVieweRef, ResultViewerProps>(({
   }, [tournament]);
 
   const paths = gameService.getOutputs(curSession!, scene);
+
   const onSelected = useCallback((index) => {
     setSelectedImageIndex(index);
   },[]);
@@ -602,6 +634,11 @@ const ResultViewer = forwardRef<ResultVieweRef, ResultViewerProps>(({
       }
     })
   };
+
+  const getPaths = () => {
+    const paths = gameService.getOutputs(curSession!, scene);
+    return selectedTab === 2 ? paths.filter((path) => isMainImage && isMainImage(path)) : paths;
+  }
 
   return (
     <div className="w-full h-full flex flex-col">
@@ -708,7 +745,7 @@ const ResultViewer = forwardRef<ResultVieweRef, ResultViewerProps>(({
                 setSelectedImageIndex(undefined);
               }}
               scene={scene}
-              paths={selectedTab === 2 ? paths.filter((path) => isMainImage && isMainImage(path)) : paths}
+              getPaths={getPaths}
               initialSelectedIndex={selectedImageIndex}
             />
           </FloatView>
